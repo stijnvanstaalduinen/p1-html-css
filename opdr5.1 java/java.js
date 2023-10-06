17a414290e670b81160d662f7d5fffc8a5ccf850
stijnvanstaalduinen <9022719@student.zadkine.nl>
2023-10-06 09:55:00 +0200

    change

--- opdr5.1 java/java.js
+++ opdr5.1 java/java.js
@@ -6,3 +6,11 @@ function verandertekst(tekst)
 {
    document.getElementById("kop1").innerHTML = "welkom"
 }
+function calculate()
+{
+let getal1 = prompt("geef getal 1", "");
+let getal2 = prompt("geef getal 2", "");
+antwoord = getal1 + getal2;
+console.log(antwoord);
+document.write("de som is:" + getal1 + "+" + getal2 + "=" + antwoord);
+}
