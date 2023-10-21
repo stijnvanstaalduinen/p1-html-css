66cf9dbbea51683f02bd7f9f42a6d4bd55532e21
stijnvanstaalduinen <9022719@student.zadkine.nl>
2023-10-21 14:52:51 +0200

    change

---
+++ jav.js
@@ -0,0 +1,16 @@
+function calculator(num, num2 ){
+let anwser = num + num2; 
+  console.log(anwser);
+}
+
+calculator(8,15);
+
+function multiplyByFive(num1, num2) {
+let anwser = num1 * num2;
+   console.log(anwser);
+}
+
+function calculateminutes(num1, num2){
+let anwser = num1 / num2;
+    console.log(anwser);
+}
--- opdr6.2 java/java.js
+++ opdr6.2 java/java.js
@@ -4,13 +4,5 @@ function veranderkleur(kleur)
 }
 function verandertekst(tekst)
 {
-   document.getElementById("kop1").innerHTML = "welkom"
-}
-function calculate()
-{
-let getal1 = prompt("geef getal 1", "");
-let getal2 = prompt("geef getal 2", "");
-antwoord = getal1 + getal2;
-console.log(antwoord);
-document.write("de som is:" + getal1 + "+" + getal2 + "=" + antwoord);
+   document.getElementById("kop1").innerHTML = "veranderd"
 }
